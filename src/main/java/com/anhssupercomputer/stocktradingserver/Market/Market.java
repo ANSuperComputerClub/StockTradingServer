e88f4c646b9db315af44d0fbb27ac8efb0f0fad5
CoolPolyester <Mkasper045@gmail.com>
2022-12-11 16:38:09 -0800

    Add outline for Market class

---
+++ src/main/java/com/anhssupercomputer/stocktradingserver/Market/Market.java
@@ -0,0 +1,15 @@
+package com.anhssupercomputer.stocktradingserver.Market;
+
+import com.anhssupercomputer.stocktradingserver.Utility.AbstractSystem;
+
+public class Market extends AbstractSystem {
+
+    public Market(int period) {
+        super(period);
+    }
+
+    @Override
+    protected void controlLoop() {
+
+    }
+}
