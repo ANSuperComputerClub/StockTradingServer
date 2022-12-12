e8b6a488a024132e6cfade86afc26a948ae50fb9
CoolPolyester <Mkasper045@gmail.com>
2022-12-11 16:38:41 -0800

    Add a trader controller to handle trader requests

--- src/main/java/com/anhssupercomputer/stocktradingserver/Trader/TraderController.java
+++ src/main/java/com/anhssupercomputer/stocktradingserver/Trader/TraderController.java
@@ -1,4 +1,39 @@
 package com.anhssupercomputer.stocktradingserver.Trader;
 
+import com.anhssupercomputer.stocktradingserver.Exceptions.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("trader")
 public class TraderController {
+    // dependency inject a traderservice
+    private final TraderService service;
+
+    public TraderController(@Autowired TraderService service) {
+        this.service = service;
+    }
+
+    /**
+     * Finds a trader with the specififed id
+     * @param id the id of the trader
+     * @return the Trader, if it is found
+     * @throws NotFoundException If the trader is not found
+     */
+    @GetMapping
+    public Trader getTrader(@RequestParam int id) throws NotFoundException {
+        return service.getTraderById(id);
+    }
+
+    @PostMapping
+    public String createTrader(@RequestBody String username, @RequestBody String key) {
+        Trader trader = new Trader(username, key);
+        service.addTrader(trader);
+        return "";
+    }
+
+    @ExceptionHandler
+    public Exception handleException(Exception e) {
+        return e;
+    }
 }
